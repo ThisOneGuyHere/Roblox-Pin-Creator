2d844d0ed13a804b1a534b3c62fa42b6d8a77c83
ThisOneGuyHere <114193855+ThisOneGuyHere@users.noreply.github.com>
2022-10-17 17:54:44 -0700

    Add files via upload

---
+++ Roblox-Pin-Creator.py
@@ -0,0 +1,7 @@
+import random
+
+num = random.randrange(0000, 9999) # Generates a random number 0000-9999
+print(num)
+print('Make sure to remember this code!')
+
+enter = input("Press enter to exit")
